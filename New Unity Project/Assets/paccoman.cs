734ee8c5d13aee6a0b098226c7aa827a62034d42
Dogaruchaff <dogaruchaff@hotmail.com>
2022-01-24 21:50:43 +0300

    ha

---
+++ New Unity Project/Assets/paccoman.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paccoman : MonoBehaviour
+{
+    void Start()
+    {
+        print("hello");
+    }
+    void Update()
+    {
+        
+    }
+}
